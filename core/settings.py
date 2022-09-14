353ab27e8329dc1905e22d327f3bda6079f16ea4
Adeniran John <adeniranjohn2016@gmail.com>
2022-09-14 13:28:47 +0100

    databse settings

--- core/settings.py
+++ core/settings.py
@@ -73,11 +73,11 @@ WSGI_APPLICATION = "core.wsgi.application"
 DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config("DB_NAME"),
-        'USER': config("DB_USER"),
-        'PASSWORD': config("DB_PASS"),
-        'HOST': config("DB_HOST"),
-        'PORT': config("DB_PORT"),
+        # 'NAME': config("DB_NAME"),
+        # 'USER': config("DB_USER"),
+        # 'PASSWORD': config("DB_PASS"),
+        # 'HOST': config("DB_HOST"),
+        # 'PORT': config("DB_PORT"),
     }       
 }
 
