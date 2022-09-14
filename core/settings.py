1221e7aceba1f1201e09d27e6c2e5edbcc8f5bec
Adeniran John <adeniranjohn2016@gmail.com>
2022-09-14 12:42:44 +0100

    Added allowed host

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['employee-crud-django.heroku.com', '127.0.0.1']
 
 
 # Application definition
